733037a55fd653d7dc131167e13a5e061ef5dfa6
asigalas <alex-sig@hotmail.com>
2017-08-02 18:19:49 +0300

    ligo meta

--- UnipiForum/App_Start/RouteConfig.cs
+++ UnipiForum/App_Start/RouteConfig.cs
@@ -12,17 +12,19 @@ namespace UnipiForum
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            var namespaces = new[] {typeof (PostsController).Namespace};
+            var namespaces = new[] { typeof(PostsController).Namespace };
 
 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" },namespaces);
-            
-            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" },namespaces);
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
+
+            routes.MapRoute("About", "about", new { controller = "Home", action = "About" }, namespaces);
 
 
-           
+
         }
     }
 }
