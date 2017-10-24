e151d21b4a30a0d36704716d4f700219d3f419f6
alexandrossss <alex-sig@hotmail.com>
2017-10-24 08:29:40 +0300

    commit

--- UnipiForum/Controllers/AuthController.cs
+++ UnipiForum/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UnipiForum.ViewModels;
 
 namespace UnipiForum.Controllers
 {
@@ -12,6 +13,12 @@ namespace UnipiForum.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+               
         }
     }
 }
--- UnipiForum/ViewModels/Auth.cs
+++ UnipiForum/ViewModels/Auth.cs
@@ -5,4 +5,11 @@ using System.Web;
 
 namespace UnipiForum.ViewModels
 {
+    public class AuthLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+}
 }
