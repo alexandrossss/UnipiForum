58f73350db79f80793de83f1cda523ca885f951b
asigalas <alex-sig@hotmail.com>
2017-12-28 12:24:48 +0200

    as
    as

--- UnipiForum/Areas/Admin/Controllers/PostsController.cs
+++ UnipiForum/Areas/Admin/Controllers/PostsController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UnipiForum.Infrastructure;
 
 namespace UnipiForum.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("posts")]
     public class PostsController : Controller
     {
         // GET: Admin/Posts
